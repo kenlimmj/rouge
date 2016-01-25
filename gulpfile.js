c4d8bb8f8223ff9fd412accf643647a610e44b71
kenlimmj <kenlimmj@gmail.com>
2016-01-25 11:35:09 -0500

    Update Gulpfile to compile both ES5 and ES6 versions. Package.json includes jsnext:main accordingly.
    Changes:
    
    - When NODE_ENV is set to `production`, compiled files will be minified and sourcemaps will be produced. Otherwise, no
      minification will be performed and no sourcemaps will be generated. Flowtypes will be stripped in both cases.
    - In accordance with Rollup's recommendations, the key `jsnext:main` is set in package.json for compatibility with
      other ES6 build pipelines.
    - `npm test` alias added to automatically call isparta for code coverage tests and mocha for unit tests
    - Babel configurations are embeddeded within package.json under the `babel` key.

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,66 @@
 'use strict';
 
 const gulp = require('gulp');
+const path = require('path');
+const runSequence = require('run-sequence');
 
+const babel = require('gulp-babel');
 const concat = require('gulp-concat');
+const foreach = require('gulp-foreach');
 const plumber = require('gulp-plumber');
 const rollup = require('gulp-rollup');
 const sourcemaps = require('gulp-sourcemaps');
 
-const babel = require('rollup-plugin-babel');
-const uglify = require('rollup-plugin-uglify');
+const rollupPlugins = {
+  babel: require('rollup-plugin-babel'),
+  uglify: require('rollup-plugin-uglify'),
+};
 
-gulp.task('bundleJS', () => {
-  gulp.src('lib/rouge.js')
+const JS_PATHS = {
+  dest: 'dist',
+  entryPoint: path.join('lib', 'rouge.js'),
+  srcGlob: path.join('lib', '*.js'),
+};
+
+function stripFlowTypes(s) {
+  return s.pipe(plumber())
+          .pipe(sourcemaps.init())
+          .pipe(babel({
+            babelrc: false,
+            comments: process.env.NODE_ENV === 'production' ? true : false,
+            compact: process.env.NODE_ENV === 'production' ? true : false,
+            plugins: ['transform-flow-strip-types'],
+            sourceMaps: process.env.NODE_ENV === 'production' ? true : false,
+          }))
+          .pipe(sourcemaps.write('.'))
+          .pipe(plumber.stop())
+          .pipe(gulp.dest(path.join(JS_PATHS.dest, 'es6')));
+}
+
+gulp.task('bundleJS_es5', () => {
+  let plugins = [rollupPlugins.babel()];
+
+  if (process.env.NODE_ENV === 'production') plugins.push(rollupPlugins.uglify());
+
+  gulp.src(JS_PATHS.entryPoint, { read: false })
       .pipe(plumber())
       .pipe(rollup({
-        format: 'umd',
+        format: 'cjs',
         sourceMap: process.env.NODE_ENV === 'production' ? true : false,
-        plugins: [
-          babel(),
-          uglify(),
-        ],
+        plugins: plugins,
       }))
       .pipe(plumber.stop())
-      .pipe(gulp.dest('dist'));
+      .pipe(gulp.dest(path.join(JS_PATHS.dest, 'es5')));
 });
 
-gulp.task('default', ['bundleJS']);
+gulp.task('bundleJS_es6', () => {
+  gulp.src(JS_PATHS.srcGlob).pipe(foreach(stripFlowTypes));
+});
+
+gulp.task('watch', () => {
+  gulp.watch(JS_PATHS.srcGlob, ['bundleJS_es5', 'bundleJS_es6']);
+});
+
+gulp.task('default', () => {
+  runSequence(['bundleJS_es5', 'bundleJS_es6'], 'watch');
+});
